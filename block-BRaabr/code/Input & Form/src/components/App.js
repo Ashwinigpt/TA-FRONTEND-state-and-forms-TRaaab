f680e509f241f812dfe7e513d938978a839c4550
Ashwini Gupta <ashwinigpt1996@gmail.com>
2022-07-13 10:48:00 +0530

    complete multiple input elements & form validation

---
+++ block-BRaabr/code/Input & Form/src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import FormValidation from './FormValidation';
+import InputElement from './InputElement';
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <FormValidation />
+        <InputElement />
+      </>
+    );
+  }
+}
+
+export default App;
