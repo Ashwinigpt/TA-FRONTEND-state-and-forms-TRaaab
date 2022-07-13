72d54f620f96583bc2dec3817c9b0f35ca1c093c
Ashwini Gupta <ashwinigpt1996@gmail.com>
2022-07-13 14:53:57 +0530

    complete Shipping & billing

---
+++ block-BRaabs/code/Shipping & billing/src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ShippingBilling from './ShippingBilling';
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <ShippingBilling />
+        
+      </>
+    );
+  }
+}
+
+export default App;
